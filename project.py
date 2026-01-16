6dd4e189e0a9229f3dd773cfc4d6a852e17def1e
arpan0073 <arpand937@gmail.com>
2026-01-16 10:20:02 +0530

    Refactor order input process in project.py
    Refactored the order input process for better clarity and functionality.

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ order_total=0
 item_1=input("Enter your first item=")
 if item_1 in menu: 
     order_total+=menu[item_1] 
-    print(f"your first item has been added to the cart") 
+    print(f"your first item has been added to the cart") #see when i add such srings after adding f that means we are writing formatted string
 else:
     print("please order from the menu only")
 another_order=input("Do you want to order another item? (yes/no)")
@@ -16,4 +16,5 @@ if another_order=='yes':
         print(f"your second item has been added to the cart") 
     else: 
         print("please order from the menu only")  
+
 print(f"Your total order amount is: Rs{order_total}") #well i am intentionllly leaving thishdere see this project ahs 2 mistakes first is you cant add more than two iems and second is you cant add more than 1piece/plate of thse same item.
